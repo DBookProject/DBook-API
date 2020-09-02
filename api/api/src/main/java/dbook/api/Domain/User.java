015785c41589399f3f3780264de3252a5281b9c9
user <namsic6460@gmail.com>
2020-09-02 20:57:41 +0900

    Added User Data

--- api/api/src/main/java/dbook/api/Domain/User.java
+++ api/api/src/main/java/dbook/api/Domain/User.java
@@ -17,14 +17,30 @@ public class User {
     @Column(nullable = false, unique = true, length = 320)
     String uEmail;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     String uPassword;
 
     @Column(nullable = true)
     String uName;
 
+    @Column(nullable = false)
+    Boolean uIsCertified;
+
+    @Column(nullable = true)
+    Integer uCertifyCode;
+
+    @Column(nullable = true)
+    String uProfileImage;
+
+    @Column(nullable = true)
+    String uLibraryImage;
+
+    @Column(nullable = true)
+    String uLibraryName;
+
     public User(String email) {
         this.setEmail(email);
+        this.setIsCertified(false);
     }
 
     public Long getId() {
@@ -43,9 +59,27 @@ public class User {
         return uEmail;
     }
 
+    public Boolean getIsCertified() { return this.uIsCertified; }
+
+    public Integer getCertifyCode() {
+        return uCertifyCode;
+    }
+
+    public String getProfileImage() { return uProfileImage; }
+
+    public String getLibraryImage() { return uLibraryImage; }
+
+    public String getLibraryName() { return uLibraryName; }
+
     public void setPassword(String password) { this.uPassword = password; }
     public void setEmail(String email) {this.uEmail = email; }
     public void setName(String name) {
         this.uName = name;
     }
+    public void setIsCertified(Boolean isCertified) { this.uIsCertified = isCertified; }
+    public void setCertifyCode(Integer certifyCode) { this.uCertifyCode = certifyCode; }
+    public void setProfileImage(String profileImage) { this.uProfileImage = profileImage; }
+    public void setLibraryImage(String libraryImage) { this.uLibraryImage = libraryImage; }
+    public void setLibraryName(String libraryName) { this.uLibraryName = libraryName; }
+
 }
--- api/api/src/main/java/dbook/api/json/LoginResponse.java
+++ api/api/src/main/java/dbook/api/json/LoginResponse.java
@@ -1,5 +1,6 @@
 package dbook.api.json;
 
+import dbook.api.Domain.User;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,18 +8,35 @@ import lombok.NoArgsConstructor;
 public class LoginResponse extends Response {
 
     String token;
+    String userName;
+    String email;
+
+    String profileImage;
+    String libraryImage;
+    String libraryName;
 
     public LoginResponse(int status, String message) {
         super(status, message);
     }
 
-    public LoginResponse(int status, String message, String token) {
+    public LoginResponse(int status, String message, String token, User user) {
         super(status, message);
+
         this.token = token;
+        this.userName = user.getName();
+        this.email = user.getEmail();
+        this.profileImage = user.getProfileImage();
+        this.libraryImage = user.getLibraryImage();
+        this.libraryName = user.getLibraryName();
     }
 
     public String getToken() {
         return this.token;
     }
+    public String getUserName() { return userName; }
+    public String getEmail() { return email; }
+    public String getProfileImage() { return profileImage; }
+    public String getLibraryImage() { return libraryImage; }
+    public String getLibraryName() { return libraryName; }
 
 }
